f7f64231d1af91b9cd464315dea8fcc63a4727ec
guo <b22790188@gmail.com>
2024-09-17 00:52:39 +0800

    [chore] add allowed origin to properties

--- src/main/java/org/example/gitmazonmasternode/config/WebConfig.java
+++ src/main/java/org/example/gitmazonmasternode/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.example.gitmazonmasternode.config;
 
+import org.example.gitmazonmasternode.repository.ServiceRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080")
+            .allowedOrigins(allowedOrigins)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
